34b82e4ccf5682a94aa56ae916c98f0f161849b9
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-07 17:21:27 -0500

    Novels.jsx add

---
+++ components/Novels.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+export default () => {
+  const [title, setTitle] = useState('')
+
+  return (
+    <div className="page">
+      <div className="title">Novels</div>
+      <div className="subtitle">A Searchable list of all your favorite novels</div>
+      <input type="text" name="search" onChange={event => setTitle(event.target.value)} />
+      <div className="title">{title}</div>
+    </div>
+  )
+}
