3b1cc692eea6ea5171464b69593b42c2e9172b68
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-07 17:12:25 -0500

    client.jsx update

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.get('/genres/:id', getGenreById)
 app.get('/novels', getAllNovels)
 app.get('/novels/:identifier', getNovelByIdOrTitle)
 
+app.all('*', (request, response) => response.sendFile(path.resolve(__dirname, 'public', 'index.html')))
+
 app.listen(1337, () => {
   console.log('Listening on port 1337...') // eslint-disable-line no-console
 })
