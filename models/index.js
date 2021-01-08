ad5a1361e79ade1bdbecc7d3096fd0f77bd93bc5
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-07 19:54:59 -0500

    index.js update more component files add

--- models/index.js
+++ models/index.js
@@ -7,11 +7,11 @@ import NovelsModel from './novels'
 import NovelsGenresModel from './novelsGenres'
 
 const environment = process.env.NODE_ENV ? process.env.NODE_ENV : 'development'
-const {
-  username, password, database, host, dialect,
-} = allConfigs[environment]
+const config = allConfigs[environment]
 
-const connection = new Sequelize(database, username, password, { host, dialect })
+
+const connection = new Sequelize(config.database, config.username, config.password, { 
+  host: config.host, dialect: config.dialect })
 
 const Authors = AuthorsModel(connection, Sequelize)
 const Genres = GenresModel(connection, Sequelize)
